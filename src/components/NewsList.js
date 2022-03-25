869369051c7d54ce29d21688a0422c94e5d7d8a4
soohyun <bmy1320@gmail.com>
2022-03-25 19:07:36 +0900

    News API Project

---
+++ src/components/NewsList.js
@@ -0,0 +1,68 @@
+import usePromise from '../lib/usePromise';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  //   const [articles, setArticles] = useState(null);
+  //   const [loading, setLoading] = useState(false);
+
+  //   useEffect(() => {
+  //     // async 를 사용하는 함수 따로 선언
+  //     const fetchData = async () => {
+  //       setLoading(true);
+  //       try {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8b04d3d4665b4a68ab9fffa5edf8f078`,
+    );
+  }, [category]);
+  //         setArticles(response.data.articles);
+  //       } catch (e) {
+  //         console.log(e);
+  //       }
+  //       setLoading(false);
+  //     };
+  //     fetchData();
+  //   }, [category]);
+
+  // 대기 중일 때
+  if (loading) {
+    return <NewsListBlock>불러오는 중...</NewsListBlock>;
+  }
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
+    return null;
+  }
+
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
